403055c0d4fe837ab6ff976b441f7f504c2b7cda
HunardeepKaur <hunar192005@gmail.com>
2025-06-09 16:10:03 +0530

    basic printing

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>Hello Hunar</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+
+  return (
+    <h1>Hello Hunar Again</h1>
+  )
+}
+
+export default App
