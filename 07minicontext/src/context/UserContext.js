45552ef9b73f696472f312d4d4865c63a6d04594
HunardeepKaur <hunar192005@gmail.com>
2025-06-25 12:31:02 +0530

    context api mini project

---
+++ 07minicontext/src/context/UserContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const UserContext=React.createContext()
+
+export default UserContext;
+
+
+// well also have to make a provider - that will provide the data , top level pe laga denge iske andar jitne bhi hain vo sare usercontext ke through sari states ka access le skte hain
